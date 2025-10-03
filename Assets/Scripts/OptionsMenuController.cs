154e2e3441210c53c1b0542e2e862b26141cb0ea
BryceLie <brycelie02@gmail.com>
2025-10-03 00:11:46 -0400

    added OptionsMenuController to dev

---
+++ Assets/Scripts/OptionsMenuController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class OptionsMenuController : MonoBehaviour
+{
+    public GameObject optionsMenu; // drag your panel here in Inspector
+    public bool isOpen = false;
+
+    void Update()
+    {
+        // Only close menu if Esc is pressed AND menu is open
+        if (Input.GetKeyDown(KeyCode.Escape) && isOpen)
+        {
+            ToggleMenu();
+        }
+    }
+
+    // This function toggles the menu open/close
+    public void ToggleMenu()
+    {
+        isOpen = !isOpen; // flip the state
+        optionsMenu.SetActive(isOpen);
+    }
+}
